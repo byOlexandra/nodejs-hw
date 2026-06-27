3ea732f1216a2f5f6e0c33a7bc85f5f74ff4ebd9
Oleksandra Starovatova <ist251_sov@student.ztu.edu.ua>
2026-06-27 18:25:25 +0300

    fix/port

--- src/server.js
+++ src/server.js
@@ -64,6 +64,8 @@ app.use((err, req, res, next) => {
   });
 });
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server is running on port ${process.env.PORT}`);
+const PORT = process.env.PORT ?? 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
