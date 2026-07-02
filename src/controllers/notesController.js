8701ebdaf7fce65116be93652d927bd96295afbf
Oleksandra Starovatova <starovatovasasha@gmail.com>
2026-07-02 11:51:48 +0300

    error Handler with http-errors lib

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -11,7 +11,7 @@ export const getNoteById = async (req, res) => {
   const note = await Note.findById(noteId);
 
   if (!note) {
-    return res.status(404).json({ message: 'Note not found' });
+    throw createHttpError(404, 'Note not found');
   }
   res.status(200).json(note);
 };
--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,5 +1,12 @@
+import { HttpError } from 'http-errors';
+
 export const errorHandler = (err, req, res, next) => {
-    res.status(500).json({
-        message: err.message,
+  if (err instanceof HttpError) {
+    return res.status(err.status).json({
+      message: err.message || err.name,
     });
+  }
+  res.status(500).json({
+    message: err.message,
+  });
 };
