960ca3dc1ca91864a6f2cd22ba94924b50c67b50
Oleksandra Starovatova <starovatovasasha@gmail.com>
2026-07-10 13:10:26 +0300

    notesValidation.js

--- src/validations/notesValidation.js
+++ src/validations/notesValidation.js
@@ -6,7 +6,7 @@ export const getAllNotesSchema = {
   [Segments.BODY]: Joi.object({
     page: Joi.number().integer().min(1).default(1),
     perPage: Joi.number().integer().min(5).max(20).default(10),
-    tag: Joi.string().valid(TAGS),
+    tag: Joi.string().valid(...TAGS),
     search: Joi.string().trim().allow(''),
   }),
 };
@@ -25,15 +25,17 @@ export const createNoteSchema = {
   [Segments.BODY]: Joi.object({
     title: Joi.string().min(1).required(),
     content: Joi.string().allow(''),
-    tag: Joi.string().valid(TAGS),
+    tag: Joi.string().valid(...TAGS),
   }),
 };
 
 export const updateNoteSchema = {
-    [Segments.PARAMS]: Joi.object({
-        ...noteIdSchema,
-    }),
-    [Segments.BODY]: Joi.object({
-        ...createNoteSchema
-    }).min(1),
+  [Segments.PARAMS]: Joi.object({
+    noteId: Joi.string().custom(objectIdValidator).required(),
+  }),
+ [Segments.BODY]: Joi.object({
+    title: Joi.string().min(1),
+    content: Joi.string().allow(''),
+    tag: Joi.string().valid(...TAGS),
+  }).min(1),
 };
