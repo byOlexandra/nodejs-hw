a669bb6be2d7696e1e4cef20b44b79888add3567
Oleksandra Starovatova <starovatovasasha@gmail.com>
2026-07-10 12:50:21 +0300

    noteIdSchema

--- src/validations/notesValidation.js
+++ src/validations/notesValidation.js
@@ -1,11 +1,22 @@
 import { Joi, Segments } from "celebrate";
 import { TAGS } from "../constants/tag.js";
+import { isValidObjectId } from "mongoose";
 
-export const getAllNotesSchema = () => {
-    [Segments.BODY] = Joi.object({
+export const getAllNotesSchema = {
+    [Segments.BODY]: Joi.object({
         page: Joi.number().integer().min(1).default(1),
         perPage: Joi.number().integer().min(5).max(20).default(10),
         tag: Joi.string().valid(TAGS),
         search: Joi.string().trim().allow(""),
-    });
+    }),
+};
+
+const objectIdValidator = (value, helpers) => {
+    return !isValidObjectId(value) ? helpers.message('Invalid id format') : value;
+};
+
+export const noteIdSchema = {
+    [Segments.PARAMS]: Joi.object({
+        noteId: Joi.string().custom(objectIdValidator).required(),
+    }),
 };
