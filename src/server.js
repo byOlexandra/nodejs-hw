90c485ca1860188434ed11577635454514efa863
Oleksandra Starovatova <starovatovasasha@gmail.com>
2026-07-12 12:29:43 +0300

    setSessionCookies(res, session)

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import cookieParser from "cookie-parser";
 import 'dotenv/config';
 import { connectMongoDB } from "./db/connectMongoDB.js";
 import { logger } from './middleware/logger.js';
@@ -14,6 +15,7 @@ const PORT = process.env.PORT ?? 3000;
 app.use(logger);
 app.use(express.json());
 app.use(cors());
+app.use(cookieParser());
 
 app.use(notesRoutes);
 
