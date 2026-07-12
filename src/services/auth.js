53976a8f54f05441f869ac8284217fc130f75ee0
Oleksandra Starovatova <starovatovasasha@gmail.com>
2026-07-12 12:25:12 +0300

    createSession(userId)

---
+++ src/services/auth.js
@@ -0,0 +1,16 @@
+import crypto from 'crypto';
+import { Session } from '../models/session.js';
+import { FIFTEEN_MINUTES, ONE_DAY } from '../constants/time.js';
+
+export const createSession = async (userId) => {
+  const accessToken = crypto.randomUUID();
+  const refreshToken = crypto.randomUUID();
+
+  return Session.create({
+    userId,
+    accessToken,
+    refreshToken,
+    accessTokenValidUntil: new Date(Date.now() + FIFTEEN_MINUTES),
+    refreshTokenValidUntil: new Date(Date.now() + ONE_DAY),
+  });
+};
