8c88395d1bcd23676575a043814bab27a4b10992
Oleksandra Starovatova <starovatovasasha@gmail.com>
2026-07-12 12:50:35 +0300

    loginUser

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,21 +1,50 @@
 import createHttpError from 'http-errors';
-import bcrypt from "bcrypt";
-import { User } from "../models/user.js";
+import bcrypt from 'bcrypt';
+import { User } from '../models/user.js';
+import { Session } from '../models/session.js';
+import { createSession, setSessionCookies } from '../services/auth.js';
 
 export const registerUser = async (req, res) => {
-    const { email, password } = req.body;
+  const { email, password } = req.body;
 
-    const existingUser = await User.findOne({ email });
-    if (existingUser) {
-        throw createHttpError(400, 'Email in use');
-    }
+  const existingUser = await User.findOne({ email });
+  if (existingUser) {
+    throw createHttpError(400, 'Email in use');
+  }
 
-    const hashedPassword = await bcrypt.hash(password, 10);
+  const hashedPassword = await bcrypt.hash(password, 10);
 
-    const newUser = await User.create({
-        email,
-        password: hashedPassword,
-    });
-    
-    res.status(201).json(newUser);
+  const user = await User.create({
+    email,
+    password: hashedPassword,
+  });
+
+  const session = await createSession(user._id);
+
+  setSessionCookies(res, session);
+
+  res.status(201).json(user);
+};
+
+export const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    throw createHttpError(401, 'Invalid credentials');
+  }
+
+  const isValidPassword = await bcrypt.compare(password, user.password);
+  if (!isValidPassword) {
+    throw createHttpError(401, 'Invalid credentials');
+  }
+
+  await Session.deleteOne({ userId: user._id });
+
+  const newSession = await createSession(user._id);
+
+  setSessionCookies(res, newSession);
+
+  res.status(200).json(user);
 };
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import { celebrate } from 'celebrate';
-import { registerUserSchema } from '../validations/authValidation';
+import { loginUserSchema, registerUserSchema } from '../validations/authValidation.js';
+import { loginUser, registerUser } from '../controllers/authController.js';
 
 const router = Router();
 
-router.post('/auth/register', celebrate(registerUserSchema), )
+router.post('/auth/register', celebrate(registerUserSchema), registerUser);
+router.post('/auth/login', celebrate(loginUserSchema), loginUser);
 
 export default router;
